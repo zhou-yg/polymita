3d9c74664273a8f903493b5a9975d12166238b1d
zhou-yg <zhou.y.g.890@gmail.com>
2023-08-02 10:45:41 +0800

    feat: upgrade drawer features

--- components/drawer/index.tsx
+++ components/drawer/index.tsx
@@ -1,4 +1,4 @@
-import { h, SignalProps, PropTypes, useLogic, ConvertToLayoutTreeDraft } from '@polymita/renderer';
+import { h, SignalProps, PropTypes, useLogic, ConvertToLayoutTreeDraft, VirtualLayoutJSON } from '@polymita/renderer';
 import { after, Signal, signal } from '@polymita/signal'
 import CloseIcon from '../../icons/close'
 
@@ -14,6 +14,8 @@ export interface DrawerProps {
   width?: number
   children?: any
   onClose?: () => void
+  closable?: boolean
+  extra?: VirtualLayoutJSON
 }
 
 export const propTypes = {
@@ -30,8 +32,9 @@ export type DrawerLayout = {
   children: [
   ]
 }
+
 export const layout = (props: DrawerProps) => {
-  const { title, width = 600, children, onClose } = props
+  const { title, width = 600, children, onClose, closable, extra } = props
   const logic = useLogic<LogicReturn>() 
   return (
     <drawerContainer className="block fixed left-0 top-0 w-full h-full" >
@@ -39,10 +42,15 @@ export const layout = (props: DrawerProps) => {
       <drawerBox className="block fixed top-0 right-0 h-full z-10 bg-white" style={{ width: `${width}px` }}>
         <drawerHeader className="flex items-center p-4 border-b" >
           {title}
+          
+          {closable ? (
+            <closeBox className="block absolute top-4 right-4 cursor-pointer" onClick={onClose} >
+              <CloseIcon color="rgba(0,0,0,.45)" />
+            </closeBox>
+          ) : ''}
 
-          <closeBox className="block absolute top-4 right-4 cursor-pointer" onClick={props.onClose} >
-            <CloseIcon color="rgba(0,0,0,.45)" />
-          </closeBox>
+          {extra || ''}
+          
         </drawerHeader>
         <drawerBody className="block overflow-auto p-4">
           {children}
